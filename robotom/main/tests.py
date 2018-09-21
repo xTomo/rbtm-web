b1439c4ee8b6a3fe00d3d05155cafe56165480da
gogbajbobo <grigoriev.maxim@gmail.com>
2018-09-21 12:29:08 +0300

    rm unused imports

--- robotom/main/tests.py
+++ robotom/main/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
-from django.forms import ValidationError
 from main.models import UserProfile
-from bootstrap3.exceptions import BootstrapError
 
 
 class LoginTest(TestCase):

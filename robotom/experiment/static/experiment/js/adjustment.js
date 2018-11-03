9eff52159d830195012563067e6c61326f9f98da
gogbajbobo <grigoriev.maxim@gmail.com>
2018-11-03 11:19:38 +0300

    extract remaining inline js to adjustment.js

--- robotom/experiment/static/experiment/js/adjustment.js
+++ robotom/experiment/static/experiment/js/adjustment.js
@@ -48,3 +48,112 @@ function post(path, params) {
     form.submit();
 
 }
+
+
+var createCORSRequest = function(method, url) {
+
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // Most browsers.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // IE8 & IE9
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+      return xhr;
+
+};
+
+
+function setSpanValue(spanId, text) {
+
+    var span = document.getElementById(spanId);
+    span.innerHTML = text;
+
+}
+
+function setValueOnload(spanId, respJSON, measure) {
+
+    try {
+
+        response = jQuery.parseJSON(respJSON);
+
+        if (response.success) {
+
+            var value_to_set = response.result;
+
+            // format the angle float value
+            if (spanId === "current_angle") {
+
+                // FORMAT ANGLE HERE!!!1!
+                value_to_set = parseFloat(value_to_set).toFixed(4);
+
+            }
+
+            // set the shutter state depending on
+            if (spanId === "current_shutter") {
+
+                // TODO: I don't understand next line, value_to_set is a dictionary, why it should be eval()-uated?
+                var dict = eval("(" + value_to_set + ")");
+
+                if (dict["state"] === "OPEN") {
+                    value_to_set = "открыта";
+                } else {
+                    value_to_set = "закрыта";
+                }
+
+            }
+
+            setSpanValue(spanId, value_to_set + measure);
+
+        } else {
+            setSpanValue(spanId, response.exception_message);
+        }
+
+    } catch (e) {
+
+        setValueOnerror(spanId);
+        return;
+
+    }
+
+}
+
+function setValueOnerror(spanId) {
+    setSpanValue(spanId, "ошибка загрузки");
+}
+
+function reloadValue(url, spanId, measure) {
+
+    var xhr = createCORSRequest('GET', url);
+
+    xhr.onload = function() {
+        setValueOnload(spanId, this.response, measure);
+    };
+
+    xhr.onerror = function() {
+        setValueOnerror(spanId);
+    };
+
+    xhr.send();
+
+}
+
+function display() {
+
+    reloadValue(get_voltage_url, "current_voltage", " кВ");
+    reloadValue(get_current_url, "current_current", " мА");
+    reloadValue(get_angle_url, "current_angle", "&deg;");
+    reloadValue(get_vert_url, "current_vert", "");
+    reloadValue(get_horiz_url, "current_horiz", "");
+    reloadValue(get_shutter_url, "current_shutter", "");
+    setTimeout("display()", 3000)
+
+}
+
+display()
+

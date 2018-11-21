f572f945c5491f82fb74a1f3b84bbb50860b89d0
gogbajbobo <grigoriev.maxim@gmail.com>
2018-11-21 11:05:26 +0300

    comment out console logs

--- robotom/storage/static/storage/js/storage_index.js
+++ robotom/storage/static/storage/js/storage_index.js
@@ -1,5 +1,5 @@
-console.log(`storage_url: ${ storage_url }`)
-console.log(`page_size: ${ page_size }`)
+//console.log(`storage_url: ${ storage_url }`)
+//console.log(`page_size: ${ page_size }`)
 
 function showPage(page) {
     var divs = $(".paginated-content");

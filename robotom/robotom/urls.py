bf4e57e65cada65492568983c0eb98b58db6d5a9
gogbajbobo <grigoriev.maxim@gmail.com>
2019-01-05 13:07:53 +0300

    urls.patterns deprecation solved

--- robotom/robotom/urls.py
+++ robotom/robotom/urls.py
@@ -1,18 +1,22 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-                       url('', include('main.urls', namespace='main')),
-                       url(r'^experiment/', include('experiment.urls', namespace='experiment')),
-                       url(r'^storage/', include('storage.urls', namespace='storage')),
 
-                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
 
-                       url(r'^accounts/', include('registration.backends.simple.urls')),
-                       url(r'^accounts/', include('django.contrib.auth.urls')),
-                       ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url('', include('main.urls', namespace='main')),
+    url(r'^experiment/', include('experiment.urls', namespace='experiment')),
+    url(r'^storage/', include('storage.urls', namespace='storage')),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
